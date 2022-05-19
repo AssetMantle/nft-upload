0b8cf26cd6422e4b996c2fdcb70d1eed7abe0692
namankabra <namanmkabra@gmail.com>
2022-05-19 19:37:21 +0530

    Updated Readme

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     nftStorageEndpoint: 'https://api.nft.storage',
-    nftStorageApiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweERkZkNkMzMwZThjQ0VkN2MxYTc3ODk5QWNiMDg1YUY0MEJCZWZhMDkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1Mjk2NjY3MTAzNCwibmFtZSI6InRlc3QtYXBpIn0.Wft5L982diBjiEuyfS1bPv91kxNj1SlRyaQJ4Ql8bfU',
+    nftStorageApiKey: 'YOUR_STORAGE_API_KEY',
     baseTokenUri: 'ipfs://'
 }
