6742abc42b14cc8ef8edfc8f81fe560b1f1fd5bd
Sarvagya Sagar <iamsarvagyaa@gmail.com>
2022-05-20 15:19:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     nftStorageEndpoint: 'https://api.nft.storage',
-    nftStorageApiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDExOUFkYjlDNGQzQUFCZGIwYzFkYWRDZkE0MTg3MWExYTlkNmFEMjIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1MjcwMDAwNjY2MiwibmFtZSI6InRlc3RpbmcifQ.8rC0gdqWVoW55fHltKeTwcDE7g3O4HLVCcTso1IPvOY',
+    nftStorageApiKey: 'INSERT_API_KEY_HERE',
     baseTokenUri: 'ipfs://'
 }
