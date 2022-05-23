4e2a3d8ec18f513b9058bba40fb39c4ffc623483
Sarvagya Sagar <iamsarvagyaa@gmail.com>
2022-05-23 15:48:55 +0530

    updates: added web3storage script

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
     nftStorageEndpoint: 'https://api.nft.storage',
-    nftStorageApiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDExOUFkYjlDNGQzQUFCZGIwYzFkYWRDZkE0MTg3MWExYTlkNmFEMjIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY1MjcwMDAwNjY2MiwibmFtZSI6InRlc3RpbmcifQ.8rC0gdqWVoW55fHltKeTwcDE7g3O4HLVCcTso1IPvOY',
-    baseTokenUri: 'ipfs://'
+    nftStorageApiKey: 'INSERT_API_KEY_HERE',
+    baseTokenUri: 'ipfs://',
+    web3StorageEndpoint: 'https://api.web3.storage',
+    web3StorageAPIKey: 'INSERT_API_KEY_HERE'
 }
