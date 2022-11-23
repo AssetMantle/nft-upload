af43b10207c5d641a127af8aa6479902ba262573
Sarvagya Sagar <iamsarvagyaa@gmail.com>
2022-11-23 19:48:39 +0530

    chore: removed config

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
   nftStorageEndpoint: "https://api.nft.storage",
-  nftStorageApiKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDhDNUEwMzhiMTJDMDM3MkVhNjMzMGZmRWFFYkFmY0E3MDZDODdhYzQiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2ODc3OTcwNTExNCwibmFtZSI6ImthbWxlc2gifQ.aigv39LfY6J7uXNkLYGMEfdwHP-3nqpYZPKteUVqdlc",
+  nftStorageApiKey: "",
   baseTokenUri: "ipfs://",
   web3StorageEndpoint: "https://api.web3.storage",
   web3StorageAPIKey: "INSERT_API_KEY_HERE",
